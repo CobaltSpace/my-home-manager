3cc1b7437f8b58bfa953e66b3131720e19dd5edc
Cobalt Space <cobaltspace@protonmail.com>
2024-11-19 16:47:12 -0800

    start migratiion of hyprland conf

--- graphical/hypr/hyprland.nix
+++ graphical/hypr/hyprland.nix
@@ -5,9 +5,34 @@
     package = lib.mkIf (builtins.pathExists /usr/bin/hyprland) (
       lib.makeOverridable ({ ... }: pkgs.emptyDirectory) { }
     );
-    xwayland.enable = false;
+    importantPrefixes = [
+      "$"
+      "bezier"
+      "env"
+      "name"
+      "source"
+    ];
     settings = {
-      source = "./hyprland.conf.bak";
+      env = [
+        "_JAVA_AWT_WM_NONREPARENTING,1"
+        "QT_QPA_PLATFORMTHEME,qt5ct"
+        "BROWSER,firefox"
+        "TERMINAL,alacritty"
+      ];
+      general = {
+        gaps_out = 0;
+        border_size = 2;
+        col = {
+          active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          inactive_border = "rgba(595959aa)";
+        };
+      };
+      input = {
+        numlock_by_default = true;
+
+        touchpad.natural_scroll = true;
+      };
+      source = [ "./hyprland.conf.bak" ];
     };
   };
 }
