dbbf8f54554f141baefe9a488f199da5a19cdf38
Cobalt Space <cobaltspace@protonmail.com>
2025-02-06 22:02:18 -0800

    swaync system package

--- graphical/swaync.nix
+++ graphical/swaync.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   services.swaync = {
     enable = true;
+    package = lib.mkIf (lib.pathExists /usr/bin/swaync) pkgs.emptyDirectory;
     settings = {
       positionX = "right";
       positionY = "bottom";
